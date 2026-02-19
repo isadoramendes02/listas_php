4416bae1c982586486bbb63bb556404c6d64a9ce
isadoramendes02 <isadora.mendes020520@gmail.com>
2026-02-19 11:51:40 -0300

    php

--- questao36.php
+++ questao36.php
@@ -3,14 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Conta de luz </title>
 </head>
 <body>
+    <h1>Conta de luz</h1>
     <h2>36. Faça um algoritmo que calcule o valor da conta de luz de uma pessoa. Sabe-se que o cálculo da conta de luz segue<br>
     a tabela abaixo:<br>
     Tipo de Cliente Valor do KW/h<br>
     1 (Residência) 0,60<br>
     2 (Comércio) 0,48<br>
     3 (Indústria) 1,29</h2><br>
+
+    <form action="" method="GET">
+        <label>Escolha o tipo de cliente:</label>
+        <select name="cliente" required>
+        <option value =""> Escolha </option>
+        <option value= "n1"> Residência </option>
+        <option value= "n2"> Comércio </option>
+        <option value= "n3"> Indústria </option>
+        </select><br><br>
+        
+    <button type="submit"> Enviar </button> 
+</form>
 </body>
 </html>
