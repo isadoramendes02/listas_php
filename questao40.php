031c7b5cb40294fc19e198f5854099848f6e6fb7
isadoramendes02 <isadora.mendes020520@gmail.com>
2026-03-10 14:29:21 -0300

    php

--- questao40.php
+++ questao40.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+-8;OCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -14,6 +14,17 @@
     Imprimir o nome a idade e a categoria do pretendente, e , caso a idade não esteja na faixa necessária, imprimir uma
     mensagem.</h2>
 
-    
+    <form action="" method="GET">
+        <label>Digite seu nome:</label>
+        <input type="text" name="nome" required><br><br>
+
+        <label>Digite sua idade:</label>
+        <input type="number" name="idade" required><br><br>
+
+        <label>Digite sua categoria:</label>
+        <input type="number" name="categoria" required><br><br>
+
+        <button type="submit"> Enviar </button>
+    </form>
 </body>
 </html>
